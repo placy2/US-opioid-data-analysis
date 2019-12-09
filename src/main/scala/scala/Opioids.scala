52103606f968f3bf879c94a17955c064560da748
Parker Lacy <placy@trinity.edu>
2019-12-08 20:36:03 -0600

    Moving forward with some tests of things

--- src/main/scala/scala/Opioids.scala
+++ src/main/scala/scala/Opioids.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 
 object Opioids {
-  def main(args: Array[String]):Unit = {
+  def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder()
       .master("spark://pandora00:7077")
@@ -22,79 +22,85 @@ object Opioids {
       .csv("/data/BigData/students/placy/arcos-api/data/buyer_annual.csv")
 
     val buyerMonthly06 = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2006.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2006.csv")
 
     val buyerMonthly07 = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2007.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2007.csv")
 
     val buyerMonthly08 = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2008.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2008.csv")
 
     val buyerMonthly09 = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2009.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2009.csv")
 
     val buyerMonthly10 = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2010.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2010.csv")
 
     val buyerMonthly11 = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2011.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2011.csv")
 
     val buyerMonthly12 = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2012.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/buyer_monthly2012.csv")
 
     val countyAnnualData = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/county_annual.csv")
-    
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/county_annual.csv")
+
     val countyMonthlyData = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/county_monthly.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/county_monthly.csv")
 
     val buyerDetailData = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/detail_list_buyers.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/detail_list_buyers.csv")
 
     val reporterDetailData = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/detail_list_reporters.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv(
+        "/data/BigData/students/placy/arcos-api/data/detail_list_reporters.csv"
+      )
 
     val pharmacyDetailData = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/pharmacies_cbsa.csv")
-    
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/pharmacies_cbsa.csv")
+
     val pharmacyLatLon = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/pharmacies_latlon.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/data/BigData/students/placy/arcos-api/data/pharmacies_latlon.csv")
 
     val countyPopulations = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/pop_counties_20062012.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv(
+        "/data/BigData/students/placy/arcos-api/data/pop_counties_20062012.csv"
+      )
 
     val statePopulations = spark.read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/data/BigData/students/placy/arcos-api/data/pop_states_20062012.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv(
+        "/data/BigData/students/placy/arcos-api/data/pop_states_20062012.csv"
+      )
 
     val stateSchema = StructType(
       Array(
@@ -114,42 +120,50 @@ object Opioids {
     )
 
     val blsAreaData = spark.read
-    .schema(areaSchema)
-    .option("header", "true")
-    .option("delimeter", "\t")
-    .csv("/data/BigData/bls/la/la.area").filter('areaType === "F" || 'areaType === "A")
+      .schema(areaSchema)
+      .option("header", "true")
+      .option("delimeter", "\t")
+      .csv("/data/BigData/bls/la/la.area")
+      .filter('areaType === "F" || 'areaType === "A")
 
     val blsStateData = (spark.read
-    .schema(stateSchema)
-    .option("header", "true")
-    .option("delimeter", "\t")
-    .csv("/data/BigData/bls/la/la.data.concatenatedStateFiles")).filter('year > 2005 && 'year < 2012)
+      .schema(stateSchema)
+      .option("header", "true")
+      .option("delimeter", "\t")
+      .csv("/data/BigData/bls/la/la.data.concatenatedStateFiles"))
+      .filter('year > 2005 && 'year < 2012)
 
 //-----------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------
     //Examining basic stats and totals
     //buyerAnnualData.agg(sum("DOSAGE_UNIT")).show()
-     // 7.663060302123816E10 total pills
-    
+    // 7.663060302123816E10 total pills
+
     //buyerMonthly12.agg(sum("DOSAGE_UNIT").alias("sum").as[Int]).select('sum).show()
-     // 12,663,969,567 pills in 2012
+    // 12,663,969,567 pills in 2012
     //buyerMonthly06.agg(sum("DOSAGE_UNIT").alias("sum").as[Int]).select('sum).show()
-     // 8,389,698,373 pills in 2006
+    // 8,389,698,373 pills in 2006
 
     //Comparing per capita pill purchases at the state level for all years
-    val renamedCounty = countyPopulations.select('BUYER_STATE.as("state"), 'year.as("popYear"))
+    val renamedStatePop =
+      statePopulations.select('BUYER_STATE.as("state"), 'year.as("popYear"))
 
-    val StateTotals = renamedCounty.join(buyerAnnualData)
+    val stateJoined = renamedStatePop
+      .join(buyerAnnualData)
       .where('state === 'BUYER_STATE && 'popYear === 'year)
-      .groupBy('state).agg(sum('DOSAGE_UNIT).as("totalState"))
-      .show(50, false)
+    //.show(50, false)
 
+    val stateGrouped = stateJoined.select(
+      'state.as[String],
+      'popYear.as[Int],
+      'DOSAGE_UNIT.as[Double],
+      'population.as[Double]
+    ).groupBy('state).agg(Map(
+      "DOSAGE_UNIT" -> sum,
+      "population" -> avg,
+    )).show(50, false)
     
-
-
-
-
 
     spark.sparkContext.stop()
     println("Application finished.")
